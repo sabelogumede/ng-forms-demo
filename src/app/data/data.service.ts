b3561c8a9b76d143c8f7d0427791efd77788d588
sabelogumede <iamsabelogumede@gmail.com>
2019-09-16 12:35:17 +0200

    handling form submission and error with observable

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // method for sorting the form
-  postUserSettingsForm(userSettings: UserSettings): Observable<UserSettings> {
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    // http post request to an online testing service (https://putsreq.com)
+    return this.http.post('https://putsreq.com/amtk4XAHHv8WvnS6ahBl', userSettings);
+
+    // return of(userSettings);
   }
 }
